0c4474483a8c03226071e9d0c595706c612bb06f
jimirings <jimirings@gmail.com>
2015-04-25 15:10:13 -0500

    Removed several unused themes

--- wp-content/themes/purple-pro/searchform.php
+++
@@ -1,5 +0,0 @@
-<form method="get" class="searchform" action="<?php echo home_url(); ?>">
-<fieldset>
-	<input type="text" value="<?php the_search_query(); ?>" name="s" /><button type="submit" name="searchsubmit" value="<?php _e('Search', 'purple_pro') ?>"><span></span></button>
-</fieldset>
-</form>
